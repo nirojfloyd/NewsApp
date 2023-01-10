1c803c57b86a9f0b98fc84d7c753f027f7703f88
Niroj Sedhai <nirojsedhai124@gmail.com>
2023-01-10 11:04:33 +0545

    Worked on ApiRequest

---
+++ app/src/main/java/com/example/newsapp/RequestManager.java
@@ -0,0 +1,44 @@
+package com.example.newsapp;
+
+
+import android.content.Context;
+
+import com.example.newsapp.Models.NewsApiResponse;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public class RequestManager {
+    Context context;
+
+    Retrofit retrofit = new Retrofit.Builder()
+            .baseUrl("https://newsapi.org/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build();
+
+
+    public void getNewsHeadlines(OnFetchDataListener listener, String category, String query ){
+
+        CallNewsApi callNewsApi=retrofit.create(CallNewsApi.class);
+        Call<NewsApiResponse> call= callNewsApi.callHeadlines("us",category,query,context.getString(R.string.api_key));
+    }
+
+    public  RequestManager(Context context){
+
+        this.context=context;
+    }
+
+
+    public interface  CallNewsApi{
+        @GET("top-headlines")
+        Call<NewsApiResponse> callHeadlines(
+                @Query("country") String country,
+                @Query("category") String category,
+                @Query("q") String query,
+                @Query("apiKey") String api_key
+        );
+    }
+}
