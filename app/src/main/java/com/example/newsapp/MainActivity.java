6f7693a9170f9d451fb03fb641b4a8d27920c1b2
Niroj Sedhai <nirojsedhai124@gmail.com>
2023-01-11 12:50:16 +0545

    Added new activity called DetailsActivity to show news' details

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.newsapp.Models.NewsApiResponse;
@@ -13,16 +15,21 @@ import com.example.newsapp.Models.NewsHeadlines;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SelectListener {
 
     RecyclerView recyclerView;
     CustomAdapter adapter;
+    ProgressDialog dialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dialog=new ProgressDialog(this);
+        dialog.setTitle("Fetching News Articles...");
+        dialog.show();
 
         RequestManager manager=new RequestManager(this);
         manager.getNewsHeadlines(listener,"general",null);
@@ -32,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onFetchData(List<NewsHeadlines> list, String message) {
             showNews(list);
+            dialog.dismiss();
         }
 
         @Override
@@ -44,9 +52,15 @@ public class MainActivity extends AppCompatActivity {
         recyclerView=findViewById(R.id.recycler_main);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this,1));
-        adapter=new CustomAdapter(this,list);
+        adapter=new CustomAdapter(this,list,this);
         recyclerView.setAdapter(adapter);
     }
 
 
+    @Override
+    public void OnNewsClicked(NewsHeadlines headlines) {
+        startActivity(new Intent(MainActivity.this,DetailsActivity.class)
+                .putExtra("data",headlines)
+        );
+    }
 }
